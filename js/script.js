0de3b3ab517eeb2ca82b3f055665a3c19bd9239b
Tania Semeshchuk <74670974+TaniaSemeshchuk@users.noreply.github.com>
2023-01-11 13:24:36 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//варіант 1
+/*
+$('#main-block').click(function(){
+    $('#sub-block').hide(1000);
+});
+
+$('#main-block').click(function(){
+    $('#sub-block').show();
+});
+*/
+
+//варіант 2
+$('#main-block').click(function(){
+    $('#sub-block').toggle(1000);
+});
+
+$('#main-block2').click(function(){
+    $('#sub-block2').toggle(1000);
+});
+
+$('#main-block3').click(function(){
+    $('#sub-block3').toggle(1000);
+});
+
+$('#main-block4').click(function(){
+    $('#sub-block4').toggle(1000);
+});
